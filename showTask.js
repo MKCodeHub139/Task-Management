9cc68d4ed23b4d20f971649f21e25ff66e3f0f0b
MKCodeHub139 <ansarikafi757@gmail.com>
2025-03-14 18:02:02 +0530

    added showTask.html,css,js with debounse search functionality and filtering table task complete/pending

--- showTask.js
+++ showTask.js
@@ -56,3 +56,113 @@ tasks.forEach(task => {
    
     
 });
+// let tr = document.querySelectorAll('tr')
+let editBtns =document.querySelectorAll('.edit-btn')
+let deleteBtns =document.querySelectorAll('.delete-btn')
+deleteBtns.forEach((deleteBtn)=>{
+    deleteBtn.addEventListener('click',(e)=>{
+        e.preventDefault()
+        let tr =e.target.closest('tr')
+        let id = parseInt(tr.querySelector('.id').innerHTML);
+        tasks =tasks.filter((task)=>task.id !== id)
+      
+        tasks = tasks.map((task, index) => ({
+            ...task,
+            id: index + 1,
+        }));
+        localStorage.setItem('tasks',JSON.stringify(tasks))
+        tr.remove();
+        if(tr.remove){
+            msg.style.display = 'flex'
+            setTimeout(() => {
+                msg.style.display = 'none'
+            }, 4000)
+        }
+        // console.log(task)
+    })
+})
+editBtns.forEach((editBtn)=>{
+    editBtn.addEventListener('click',(e)=>{
+        e.preventDefault()
+        let tr=e.target.closest('tr')
+        // window.location.assign('./editTask.html')
+        let editTaskId=tr.querySelector('.id').innerHTML
+        let editTaskTitle=tr.querySelector('.title').innerHTML
+        let editTaskDesc=tr.querySelector('.desc').innerHTML
+        let editProcess=tr.querySelector('.complete-pending').innerHTML
+        let editProcessVal =''
+
+        if(editProcess==='complete'){
+            console.log('complete')
+            editProcessVal =true
+        }
+        else{
+            console.log('pending')
+            editProcessVal =false
+            
+        }
+        let editTask ={
+            editTaskId:editTaskId,
+            editTaskTitle:editTaskTitle,
+            editTaskDesc:editTaskDesc,
+            editProcessVal:editProcessVal,
+        }
+        localStorage.setItem('editTask',JSON.stringify(editTask))
+        window.location.assign('./editTask.html')
+    })
+})
+
+// search logic
+
+let search =document.querySelector('.search')
+let searchInput =search.querySelector('input')
+
+searchInput.addEventListener('keyup',(e)=>{
+    e.preventDefault()
+    let searchVal = searchInput.value.trim().toLowerCase()
+
+        let taskSearch=tasks.filter(task=>task.title.toLowerCase().includes(searchVal) || task.desc.toLowerCase().includes(searchVal))
+
+        tbody.innerHTML=''
+        taskSearch.forEach(task=>{
+            renderTasks(task)
+        })
+
+})
+let taskFilterUl=document.querySelector('.task-filter ul')
+let taskFilterActiveLis = taskFilterUl.querySelectorAll('li')
+taskFilterActiveLis.forEach(taskFilterActiveLi=>{
+    taskFilterActiveLi.addEventListener('click', (e)=>{
+        let activeLi =taskFilterUl.querySelector('.active-filter')
+                if(activeLi){
+                    activeLi.classList.remove('active-filter')
+                }
+            e.target.classList.add('active-filter')
+    
+    })
+})
+// show pending
+
+let pendingTasksBtn =taskFilterUl.querySelector('.pending')
+let completeTasksBtn =taskFilterUl.querySelector('.complete')
+let allTasksBtn =taskFilterUl.querySelector('.all')
+
+
+pendingTasksBtn.addEventListener('click', () =>{
+    tbody.innerHTML=''
+    let taskFilter = tasks.filter(task=>task.processChecked === false)
+    taskFilter.forEach(task=>renderTasks(task))
+    
+    console.log(taskFilter)
+})
+completeTasksBtn.addEventListener('click', () =>{
+    tbody.innerHTML=''
+    let taskFilter = tasks.filter(task=>task.processChecked === true)
+    taskFilter.forEach(task=>renderTasks(task))
+    
+    console.log(taskFilter)
+})
+allTasksBtn.addEventListener('click', () =>{
+    tbody.innerHTML=''
+    tasks.forEach(task=>renderTasks(task))
+})
